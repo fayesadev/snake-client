3074fec23ffc0ccc21bb4dc87ce6cce017182a58
fayesadev <fayedumbrigue@gmail.com>
2023-01-18 23:25:17 +0000

    Implemented setupInput which takes in user input and sends to server movement commands

--- client.js
+++ client.js
@@ -8,23 +8,25 @@ const connect = function () {
 
   conn.on("connect", () => {
     console.log("Successfully connected to game server.");
-    conn.write("Name: FAY");
+    conn.write("Name: FAY"); //My name
     // conn.write("Move: up");
   })
-//Experimenting with movement at connect
+
+  //Experimenting with movement at connect
   // conn.on("connect", () => {
   //   setInterval(()=>{conn.write("Move: up")}, 50);
   //   // setTimeout(()=>{conn.write("Move: left")}, 150);
   // })
 
-//Ability to receive data from the server
-  conn.on("data", (data) => {
-    console.log(data);
-  })
+  //Ability to receive data from the server
+  // conn.on("data", (data) => {
+  //   console.log(data);
+  // })
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
   return conn;
 };
+
 
 module.exports = connect;
--- play.js
+++ play.js
@@ -1,5 +1,33 @@
 const connect = require("./client");
 
 console.log("Connecting ...");
-connect();
+const conn = connect();
 
+// setup interface to handle user input from stdin
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  const handleUserInput = function(key) {
+    if (key === "w") {
+      conn.write("Move: up");
+    }
+    if (key === "a") {
+      conn.write("Move: left");
+    }
+    if (key === "d") {
+      conn.write("Move: right");
+    }
+    if (key === "s") {
+      conn.write("Move: down");
+    }
+    if (key === '\u0003') {
+      process.exit();
+   }
+  }
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+setupInput();
