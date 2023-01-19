3cd4b15f78fe346b7caf3c319644ece7063062d8
fayesadev <fayedumbrigue@gmail.com>
2023-01-19 01:22:05 +0000

    passed conn in setupInput to link keyboard input commands to connection to the server

--- input.js
+++ input.js
@@ -1,25 +1,31 @@
 // setup interface to handle user input from stdin
 const {connect} = require('./client');
 
-const conn = connect();
+let connection;
+
 const handleUserInput = function(key) {
   if (key === "w") {
-    conn.write("Move: up");
+    // conn.write("Move: up");
+    console.log("Move: up");
   }
   if (key === "a") {
-    conn.write("Move: left");
+    // conn.write("Move: left");
+    console.log("Move: left");
   }
   if (key === "d") {
-    conn.write("Move: right");
+    // conn.write("Move: right");
+    console.log("Move: right");
   }
   if (key === "s") {
-    conn.write("Move: down");
+    // conn.write("Move: down");
+    console.log("Move: down");
   }
   if (key === '\u0003') {
     process.exit();
  }
 }
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
