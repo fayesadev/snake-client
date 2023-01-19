01d64c5f41890e58b48be5d690ea38d360f4605e
fayesadev <fayedumbrigue@gmail.com>
2023-01-19 01:53:17 +0000

    passed conn into setupInput to access connection within input.js

--- input.js
+++ input.js
@@ -1,38 +1,44 @@
 // setup interface to handle user input from stdin
 const {connect} = require('./client');
-
+// Stores the active TCP connection object.
 let connection;
 
 const handleUserInput = function(key) {
   if (key === "w") {
-    // conn.write("Move: up");
-    console.log("Move: up");
+    connection.write("Move: up");
+    // console.log("Move: up");
   }
   if (key === "a") {
-    // conn.write("Move: left");
-    console.log("Move: left");
+    connection.write("Move: left");
+    // console.log("Move: left");
   }
   if (key === "d") {
-    // conn.write("Move: right");
-    console.log("Move: right");
+    connection.write("Move: right");
+    // console.log("Move: right");
   }
   if (key === "s") {
-    // conn.write("Move: down");
-    console.log("Move: down");
+    connection.write("Move: down");
+    // console.log("Move: down");
   }
   if (key === "m") {
-    console.log("Say: Try harder!");
+    // console.log("Say: Try harder!");
+    connection.write("Say: Better luck next time!")
   }
   if (key === '\u0003') {
     process.exit();
  }
 }
+
 const setupInput = function (conn) {
   connection = conn;
+  // console.log("conn", conn);
+  // console.log("connection", connection);
+  // console.log("connect", connect);
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
+
   stdin.on("data", handleUserInput);
   return stdin;
 };
